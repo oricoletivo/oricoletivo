eed3fb7c4dda768ec04deed69b36acbc57f3b536
unknown <Cruzanaa>
2020-05-18 12:14:24 -0300

    [Ana] modified date in countdown

--- src/main.js
+++ src/main.js
@@ -1,33 +1,5 @@
-
-// var countDate = new Date('Mar 20, 2020 00:00:00').getTime();
-
-// function newYear() {
-//     var now = new Date().getTime();
-//         gap = countDate - now;
-
-//     var second = 1000;
-//     var minute = second * 60;
-//     var hour = minute * 60;
-//     var day = hour * 24;
-
-//     var d = Math.floor(gap / (day));
-//     var h = Math.floor((gap % (day)) / (hour));
-//     var m = Math.floor((gap % (hour)) / (minute));
-//     var s = Math.floor((gap % (minute)) / second);
-
-//     document.getElementById('day').innerHTML = d;
-//     document.getElementById('hour').innerHTML = h;
-//     document.getElementById('minute').innerHTML = m;
-//     document.getElementById('second').innerHTML = s;
-// }
-
-// setInterval(function() {
-//     newYear();
-//     }, 1000);
-
-
-
-var target_date = new Date("may 22, 2020").getTime();
+/* -- Contagem Regressiva --*/
+var target_date = new Date("may 20, 2020").getTime();
 var dias;
 let horas;
 let minutos;
